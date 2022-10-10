205d7fb43a29e7343c455a1837897037ad08f44a
poy <ghhh7731 @email.com>
2022-10-10 20:17:21 +0700

    form2

--- lib/screen/formscreen.dart
+++ lib/screen/formscreen.dart
@@ -1,1 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FormScreen extends StatefulWidget {
+  const FormScreen({Key key}) : super(key: key);
+
+  @override
+  State<FormScreen> createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  final formKey = GlobalKey<FormState>();
+  Money myMoney = Money();
+  final Future<FirebaseApp> firebase = Firebase.initializeApp();
+  CollectionReference _moneyCollection = FirebaseFirestore.instance.collection("Money");
+
